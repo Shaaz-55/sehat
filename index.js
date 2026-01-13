add5a38f1ffb49cb844ddecb117228c127e7fab1
shaaz-IND <shaaz23hemani@gmail.com>
2026-01-13 16:52:17 +0530

    added animations

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function changeIcon(){
-const change_icon = document.getElementsByClassName('icon');
-change_icon.innerHTML = "dq wiudvd";
-};
+// function changeIcon(){
+// const change_icon = document.getElementsByClassName('icon');
+// change_icon.innerHTML = "dq wiudvd";
+// };
+
+// const button = document.querySelector('.new_button');
+// const js_test = document.querySelector('.js_test');
+
+// button.addEventListener ('click'.function(){
+//       const newDiv = document.createElement('div');
+      
+//       newDiv.textContent = 'This is a new div!';
+//       newDiv.classList.add('new-div');
+//       container.appendChild(newDiv)
+// });
+
+
+const chat_button = document.getElementById('chat_button');
+const chat_box = document.getElementById('chat_box');
+
+chat_button.addEventListener('click',function(){
+      chat_box.classList.toggle('hidden');
+      chat_box.classList.toggle('visible');
+})
